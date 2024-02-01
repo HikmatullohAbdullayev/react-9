19a367b5d15729d656719b4d28b06fc0bfda749e
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-02-01 05:59:02 +0500

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import React, { useState } from 'react'
+import Modal from './modal/modal'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isModalOpenImg, setIsModalOpenImg] = useState(false)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main className="mx-auto w-[1220px] px-5   ">
+      <section className='content'>
+        <button className='w-full p-5 bg-teal-400 rounded-lg m-5 ' onClick={() => setIsModalOpen(true)}>
+          Modalni ochish
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <button className='w-full p-5 bg-teal-400 rounded-lg m-5 ' onClick={() => setIsModalOpenImg(true)}>
+          Modalni ochish 2
+        </button>
+    
+        {isModalOpen && (
+         <div className="  bg-amber-100 rounded-lg">
+           <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+          <div className="flex flex-col gap-4 bg-amber-100 p-5">
+          <h1 className='p-4 bg-amber-300 rounded-lg font-semibold text-2xl'>Birinchi modal</h1>
+            <p className='p-4 bg-amber-400 rounded-lg font-medium text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex distinctio facilis repellat dignissimos quis necessitatibus ullam, fugit architecto? Facere porro nobis illum error sed rem atque saepe nostrum magni vel!
+            </p>
+            <p className='p-4 bg-amber-400 rounded-lg font-medium text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex distinctio facilis repellat dignissimos quis necessitatibus ullam, fugit architecto? Facere porro nobis illum error sed rem atque saepe nostrum magni vel!
+            </p>
+          </div>
+          </Modal>
+           
+        
+         </div>
+        )}
+        {isModalOpenImg && (
+         <div className="rounded-lg  bg-lime-200">
+          <div className="flex flex-col gap-4 bg-lime-300 p-5">
+           <Modal isOpen={isModalOpenImg} onClose={() => setIsModalOpenImg(false)}>
+            <h1 className='p-4 bg-lime-500 font-semibold text-2xl rounded-lg'>Birinchi modal</h1>
+            <p className='p-4 m-4 bg-lime-400 font-medium text-lg rounded-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex distinctio facilis repellat dignissimos quis necessitatibus ullam, fugit architecto? Facere porro nobis illum error sed rem atque saepe nostrum magni vel!
+            </p>
+            
+            <img className='w-[450px] mx-auto bg-lime-300 p-5 rounded-lg' src="https://images.pexels.com/photos/386000/pexels-photo-386000.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+          </Modal></div>
+           
+        
+         </div>
+        )}
+        
+      </section>
+    </main>
   )
 }
 
---
+++ src/modal/modal.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "../App.css"
+
+function Modal({ isOpen, onClose,  children}) {
+    const onWrapperClik = (event) => {
+        if (
+          event.target.classList.contains(
+            "modal_wrapper"
+            //absolute left-0 top-0 flex justify-center items-center w-full min-h-full/
+          )
+        )
+          onClose();
+      };
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 overflow-hidden overflow-y-auto z-50 flex flex-col gap-4"  >
+          <div className="modal_wrapper" onClick={onWrapperClik}>
+            <div
+              className="relative m-5 w-full max-w-[700px] rounded-lg bg-white p-5 pt-10 translate-y-[-50px]"
+              
+            >
+              <button
+                className="absolute top-4 right-4 w-8 h-8  p-1 text-black opacity-100"
+                onClick={() => onClose()}
+              >
+                X
+              </button>
+              <div className="box">{children}</div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Modal;
+
+
+
+
+
+ 
+
+
+
+// // Modal.js
+// import React from "react";
+
+// function Modal({ isOpen, onClose, children }) {
+//   return (
+//     <>
+//       {isOpen && (
+//         <div className="absolute left-0 top-0 flex justify-center items-center w-full min-h-full">
+//           <div className="relative m-5 w-full max-w-[700px] rounded-lg bg-white p-5 pt-10 translate-y-[-50px]">
+//             <button
+//               className="absolute top-4 right-4 w-8 h-8 p-1 text-black opacity-100"
+//               onClick={() => onClose()}
+//             >
+//               X
+//             </button>
+//             <div className="box">{children}</div>
+//           </div>
+//         </div>
+//       )}
+//     </>
+//   );
+// }
+
+// export default Modal;
