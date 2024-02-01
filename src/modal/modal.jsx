6720ddb0402605b589a84defb92086cdd96939a8
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-02-01 06:02:46 +0500

    button fix

--- src/modal/modal.jsx
+++ src/modal/modal.jsx
@@ -14,14 +14,14 @@ function Modal({ isOpen, onClose,  children}) {
   return (
     <>
       {isOpen && (
-        <div className="fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 overflow-hidden overflow-y-auto z-50 flex flex-col gap-4"  >
+        <div className="fixed left-0 top-0  w-full h-full bg-black bg-opacity-50 overflow-hidden overflow-y-auto z-50 flex flex-col gap-4"  >
           <div className="modal_wrapper" onClick={onWrapperClik}>
             <div
               className="relative m-5 w-full max-w-[700px] rounded-lg bg-white p-5 pt-10 translate-y-[-50px]"
               
             >
               <button
-                className="absolute top-4 right-4 w-8 h-8  p-1 text-black opacity-100"
+                className="absolute top-1 right-4 w-8 h-8  p-1 rounded-lg bg-slate-300 text-black opacity-100"
                 onClick={() => onClose()}
               >
                 X
